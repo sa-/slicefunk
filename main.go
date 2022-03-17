149304ac6261c4aa219bf75bd33fa8ef20b030ad
sa- <samay.kapadia@solytic.com>
2022-03-17 09:54:34 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package slicefunk
+
+func Map[T, U any](s []T, f func(T) U) []U {
+	r := make([]U, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+func Filter[T any](s []T, f func(T) bool) []T {
+	r := make([]T, len(s))
+	counter := 0
+	for i := 0; i < len(s); i++ {
+		if f(s[i]) {
+			r[counter] = s[i]
+			counter++
+		}
+	}
+	return r[:counter]
+}
