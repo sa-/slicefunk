f889c3eac44b8d41fd6f5ff6bfddbb2e4f9d7e3f
Samay Kapadia <samay.kapadia@gmail.com>
2022-12-27 00:50:54 +0100

    add Flatten and FlatMap

--- main.go
+++ main.go
@@ -21,13 +21,34 @@ func Filter[T any](s []T, f func(T) bool) []T {
 }
 
 func Unique[T comparable](s []T) []T {
-    inResult := make(map[T]bool)
-    var result []T
-    for _, str := range s {
-        if _, ok := inResult[str]; !ok {
-            inResult[str] = true
-            result = append(result, str)
-        }
-    }
-    return result
+	inResult := make(map[T]bool)
+	var result []T
+	for _, str := range s {
+		if _, ok := inResult[str]; !ok {
+			inResult[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+}
+
+func Flatten[T any](s [][]T) []T {
+	itemCount := 0
+	for i := 0; i < len(s); i++ {
+		itemCount = itemCount + len(s[i])
+	}
+
+	r := make([]T, itemCount)
+	idx := 0
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s[i]); j++ {
+			r[idx] = s[i][j]
+			idx++
+		}
+	}
+	return r
+}
+
+func FlatMap[T, U any](s [][]T, f func(T) U) []U {
+	return Flatten(Map(s, func(t []T) []U { return Map(t, f) }))
 }
